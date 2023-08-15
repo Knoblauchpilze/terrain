28a1e87aa6944206584419ce5396ada802e0bee9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-08-15 21:12:58 +0200

    feat: Links

--- src/lib/game/terrain/lattice/Hasher.cc
+++ src/lib/game/terrain/lattice/Hasher.cc
@@ -9,6 +9,7 @@ Hasher::Hasher(const noise::Seed seed) noexcept
 
 auto Hasher::hash(const int x, const int y) -> float
 {
+  // // https://gamedev.stackexchange.com/questions/183142/how-can-i-create-a-persistent-seed-for-each-chunk-of-an-infinite-procedural-worl
   const auto px = (x < 0 ? -2 * x + 1 : 2 * x);
   const auto py = (y < 0 ? -2 * y + 1 : 2 * y);
 
