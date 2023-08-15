ebe6451b6225a435ff8b3eb134ff8e39ce68366b
KnoblauchPilze <prolific-programmer@tuta.com>
2023-08-15 21:12:58 +0200

    feat: Links

--- src/lib/game/terrain/lattice/ValueLattice.cc
+++ src/lib/game/terrain/lattice/ValueLattice.cc
@@ -19,13 +19,13 @@ auto ValueLattice::at(const float x, const float y) -> float
 
 auto ValueLattice::generateLatticePointsAndInterpolate(const float x, const float y) -> float
 {
+  // https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/procedural-patterns-noise-part-1/creating-simple-1D-noise.html
   const auto minX = static_cast<int>(std::floor(x));
   const auto maxX = static_cast<int>(std::ceil(x + 0.5f));
 
   const auto minY = static_cast<int>(std::floor(y));
   const auto maxY = static_cast<int>(std::ceil(y + 0.5f));
 
-  // https://gamedev.stackexchange.com/questions/183142/how-can-i-create-a-persistent-seed-for-each-chunk-of-an-infinite-procedural-worl
   auto latticeValue = [this](const int x, const int y) -> float {
     m_noise->seed(m_hasher->hash(x, y));
     return m_noise->next();
