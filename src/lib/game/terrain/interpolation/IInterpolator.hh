29ae2a48def53bddb07ffffb1f7a3f3aeaf58da6
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-08-15 20:47:43 +0200

    fix: Doc

--- src/lib/game/terrain/interpolation/IInterpolator.hh
+++ src/lib/game/terrain/interpolation/IInterpolator.hh
@@ -10,6 +10,17 @@ class IInterpolator
   public:
   virtual ~IInterpolator() = default;
 
+  /// @brief - Interpolate the value given the lattice points in argument using a
+  /// bilinear interpolation algorithm.
+  /// @param tl - the top left corner of the lattice.
+  /// @param tr - the top right corner of the lattice.
+  /// @param br - the bottom right corner of the lattice.
+  /// @param bl - the bottom left corner of the lattice.
+  /// @param px - the position of the point to interpolate along the top axis. This
+  /// value must lie in the range [0; 1].
+  /// @param py - the position of the point to interpolate along the bottom axis. This
+  /// value must lie in the range [0; 1].
+  /// @return - the interpolated value based on the input parameters.
   virtual auto interpolate(const float tl,
                            const float tr,
                            const float br,
