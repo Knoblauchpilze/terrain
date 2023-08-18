c78e9c408c0548f77f5469072e20a860017b7cf9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-08-18 08:53:14 +0200

    feat: Renamed tests and added test for generateFor

--- tests/unit/lib/game/terrain/lattice/ValueGeneratorTest.cc
+++ tests/unit/lib/game/terrain/lattice/ValueGeneratorTest.cc
@@ -8,7 +8,7 @@
 using namespace ::testing;
 
 namespace pge::lattice {
-class Unit_ValueGenerator : public GeneratorPreparer<ValueGenerator>, public Test
+class Unit_Lattice_ValueGenerator : public GeneratorPreparer<ValueGenerator>, public Test
 {
   protected:
   void SetUp() override
@@ -17,17 +17,24 @@ class Unit_ValueGenerator : public GeneratorPreparer<ValueGenerator>, public Tes
   }
 };
 
-TEST_F(Unit_ValueGenerator, Test_UseHasher)
+TEST_F(Unit_Lattice_ValueGenerator, Test_UseHasher)
 {
   EXPECT_CALL(*mockHasher, hash(_, _)).Times(1);
   generator->generateFor(utils::Vector2i(), utils::Vector2f());
 }
 
-TEST_F(Unit_ValueGenerator, Test_UseNoise)
+TEST_F(Unit_Lattice_ValueGenerator, Test_UseNoise)
 {
   EXPECT_CALL(*mockNoise, seed(_)).Times(1);
   EXPECT_CALL(*mockNoise, next()).Times(1);
   generator->generateFor(utils::Vector2i(), utils::Vector2f());
+}
+
+TEST_F(Unit_Lattice_ValueGenerator, Test_GenerateFor)
+{
+  ON_CALL(*mockNoise, next()).WillByDefault(Return(1.0f));
+  const auto actual = generator->generateFor(utils::Vector2i(), utils::Vector2f());
+  EXPECT_EQ(1.0f, actual);
 }
 
 } // namespace pge::lattice
