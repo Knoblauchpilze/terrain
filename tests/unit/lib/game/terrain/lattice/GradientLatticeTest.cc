36ced900f2d1732c4b176588e4b5ee646af6e796
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-08-20 11:32:49 +0200

    feat: Tests for actual output of lattices

--- tests/unit/lib/game/terrain/lattice/GradientLatticeTest.cc
+++ tests/unit/lib/game/terrain/lattice/GradientLatticeTest.cc
@@ -1,6 +1,9 @@
 
 #include "GradientLattice.hh"
+#include "Bilinear.hh"
+#include "Hasher.hh"
 #include "ILatticePreparer.hh"
+#include "WhiteNoise.hh"
 #include <gtest/gtest.h>
 
 using namespace ::testing;
@@ -35,7 +38,7 @@ TEST_F(Unit_Lattice_GradientLattice, Test_UseInterpolate)
 }
 
 namespace {
-struct TestCase
+struct TestCaseInterpolate
 {
   float x;
   float y;
@@ -45,7 +48,7 @@ struct TestCase
 };
 
 class InterpolateGradientTestSuite : public LatticePreparer<GradientLattice>,
-                                     public TestWithParam<TestCase>
+                                     public TestWithParam<TestCaseInterpolate>
 {
   protected:
   void SetUp() override
@@ -54,7 +57,7 @@ class InterpolateGradientTestSuite : public LatticePreparer<GradientLattice>,
   }
 };
 
-auto generateTestName(const TestParamInfo<TestCase> &info) -> std::string
+auto generateTestNameForInterpolate(const TestParamInfo<TestCaseInterpolate> &info) -> std::string
 {
   auto str = std::to_string(info.param.x) + "x" + std::to_string(info.param.y);
   std::replace(str.begin(), str.end(), '.', '_');
@@ -72,13 +75,62 @@ TEST_P(InterpolateGradientTestSuite, Test_Interpolate)
 
 INSTANTIATE_TEST_SUITE_P(Unit_Lattice_GradientLattice,
                          InterpolateGradientTestSuite,
-                         Values(TestCase{0.0f, 0.0f, 0.0f, 0.0f},
-                                TestCase{0.5f, 0.0f, 0.5f, 0.0f},
-                                TestCase{0.0f, 0.5f, 0.0f, 0.5f},
-                                TestCase{0.2f, 0.3f, 0.2f, 0.3f},
-                                TestCase{0.7f, 0.1f, 0.7f, 0.1f},
-                                TestCase{0.9f, 0.51f, 0.9f, 0.51f},
-                                TestCase{0.02f, 0.98f, 0.02f, 0.98f}),
-                         generateTestName);
+                         Values(TestCaseInterpolate{0.0f, 0.0f, 0.0f, 0.0f},
+                                TestCaseInterpolate{0.5f, 0.0f, 0.5f, 0.0f},
+                                TestCaseInterpolate{0.0f, 0.5f, 0.0f, 0.5f},
+                                TestCaseInterpolate{0.2f, 0.3f, 0.2f, 0.3f},
+                                TestCaseInterpolate{0.7f, 0.1f, 0.7f, 0.1f},
+                                TestCaseInterpolate{0.9f, 0.51f, 0.9f, 0.51f},
+                                TestCaseInterpolate{0.02f, 0.98f, 0.02f, 0.98f}),
+                         generateTestNameForInterpolate);
+
+namespace {
+constexpr auto REASONABLE_COMPARISON_THRESHOLD = 0.0001f;
+
+struct TestCaseValue
+{
+  float x;
+  float y;
+
+  float expected;
+  float threshold{REASONABLE_COMPARISON_THRESHOLD};
+};
+
+using GradientTestSuite = TestWithParam<TestCaseValue>;
+
+auto generateTestNameForValue(const TestParamInfo<TestCaseValue> &info) -> std::string
+{
+  auto str = std::to_string(info.param.x) + "x" + std::to_string(info.param.y);
+  std::replace(str.begin(), str.end(), '.', '_');
+  return str;
+}
+
+TEST_P(GradientTestSuite, Test_Value)
+{
+  const auto param = GetParam();
+
+  constexpr auto SEED = 1993;
+  auto hasher         = std::make_unique<lattice::Hasher>(SEED);
+  auto noise          = std::make_unique<noise::WhiteNoise>();
+  auto interpolator   = std::make_unique<interpolation::Bilinear>();
+  auto lattice        = std::make_unique<lattice::GradientLattice>(std::move(hasher),
+                                                            std::move(noise),
+                                                            std::move(interpolator));
+
+  const auto actual = lattice->at(param.x, param.y);
+  EXPECT_NEAR(actual, param.expected, param.threshold);
+}
+} // namespace
+
+INSTANTIATE_TEST_SUITE_P(Unit_Lattice_GradientLattice,
+                         GradientTestSuite,
+                         Values(TestCaseValue{0.0f, 0.0f, 0.5f},
+                                TestCaseValue{0.0f, 1.0f, 0.5f},
+                                TestCaseValue{0.5f, 0.5f, 0.4747550f},
+                                TestCaseValue{0.1f, 0.32f, 0.3119567f},
+                                TestCaseValue{0.49f, 0.98f, 0.4773225f},
+                                TestCaseValue{0.67f, 0.51f, 0.5752330f},
+                                TestCaseValue{0.01f, 0.79f, 0.3415382f}),
+                         generateTestNameForValue);
 
 } // namespace pge::lattice
--- tests/unit/lib/game/terrain/lattice/ValueLatticeTest.cc
+++ tests/unit/lib/game/terrain/lattice/ValueLatticeTest.cc
@@ -1,6 +1,9 @@
 
 #include "ValueLattice.hh"
+#include "Bilinear.hh"
+#include "Hasher.hh"
 #include "ILatticePreparer.hh"
+#include "WhiteNoise.hh"
 #include <gtest/gtest.h>
 
 using namespace ::testing;
@@ -35,7 +38,7 @@ TEST_F(Unit_Lattice_ValueLattice, Test_UseInterpolate)
 }
 
 namespace {
-struct TestCase
+struct TestCaseForInterpolate
 {
   float x;
   float y;
@@ -45,7 +48,7 @@ struct TestCase
 };
 
 class InterpolateValueTestSuite : public LatticePreparer<ValueLattice>,
-                                  public TestWithParam<TestCase>
+                                  public TestWithParam<TestCaseForInterpolate>
 {
   protected:
   void SetUp() override
@@ -54,7 +57,8 @@ class InterpolateValueTestSuite : public LatticePreparer<ValueLattice>,
   }
 };
 
-auto generateTestName(const TestParamInfo<TestCase> &info) -> std::string
+auto generateTestNameForInterpolate(const TestParamInfo<TestCaseForInterpolate> &info)
+  -> std::string
 {
   auto str = std::to_string(info.param.x) + "x" + std::to_string(info.param.y);
   std::replace(str.begin(), str.end(), '.', '_');
@@ -72,13 +76,62 @@ TEST_P(InterpolateValueTestSuite, Test_Interpolate)
 
 INSTANTIATE_TEST_SUITE_P(Unit_Lattice_ValueLattice,
                          InterpolateValueTestSuite,
-                         Values(TestCase{0.0f, 0.0f, 0.0f, 0.0f},
-                                TestCase{0.5f, 0.0f, 0.5f, 0.0f},
-                                TestCase{0.0f, 0.5f, 0.0f, 0.5f},
-                                TestCase{0.2f, 0.3f, 0.2f, 0.3f},
-                                TestCase{0.7f, 0.1f, 0.7f, 0.1f},
-                                TestCase{0.9f, 0.51f, 0.9f, 0.51f},
-                                TestCase{0.02f, 0.98f, 0.02f, 0.98f}),
-                         generateTestName);
+                         Values(TestCaseForInterpolate{0.0f, 0.0f, 0.0f, 0.0f},
+                                TestCaseForInterpolate{0.5f, 0.0f, 0.5f, 0.0f},
+                                TestCaseForInterpolate{0.0f, 0.5f, 0.0f, 0.5f},
+                                TestCaseForInterpolate{0.2f, 0.3f, 0.2f, 0.3f},
+                                TestCaseForInterpolate{0.7f, 0.1f, 0.7f, 0.1f},
+                                TestCaseForInterpolate{0.9f, 0.51f, 0.9f, 0.51f},
+                                TestCaseForInterpolate{0.02f, 0.98f, 0.02f, 0.98f}),
+                         generateTestNameForInterpolate);
+
+namespace {
+constexpr auto REASONABLE_COMPARISON_THRESHOLD = 0.0001f;
+
+struct TestCaseValue
+{
+  float x;
+  float y;
+
+  float expected;
+  float threshold{REASONABLE_COMPARISON_THRESHOLD};
+};
+
+using ValueTestSuite = TestWithParam<TestCaseValue>;
+
+auto generateTestNameForValue(const TestParamInfo<TestCaseValue> &info) -> std::string
+{
+  auto str = std::to_string(info.param.x) + "x" + std::to_string(info.param.y);
+  std::replace(str.begin(), str.end(), '.', '_');
+  return str;
+}
+
+TEST_P(ValueTestSuite, Test_Value)
+{
+  const auto param = GetParam();
+
+  constexpr auto SEED = 1993;
+  auto hasher         = std::make_unique<lattice::Hasher>(SEED);
+  auto noise          = std::make_unique<noise::WhiteNoise>();
+  auto interpolator   = std::make_unique<interpolation::Bilinear>();
+  auto lattice        = std::make_unique<lattice::ValueLattice>(std::move(hasher),
+                                                         std::move(noise),
+                                                         std::move(interpolator));
+
+  const auto actual = lattice->at(param.x, param.y);
+  EXPECT_NEAR(actual, param.expected, param.threshold);
+}
+} // namespace
+
+INSTANTIATE_TEST_SUITE_P(Unit_Lattice_ValueLattice,
+                         ValueTestSuite,
+                         Values(TestCaseValue{0.0f, 0.0f, 0.1004222f},
+                                TestCaseValue{0.0f, 1.0f, 0.6273638f},
+                                TestCaseValue{0.5f, 0.5f, 0.4484194f},
+                                TestCaseValue{0.1f, 0.32f, 0.2887946f},
+                                TestCaseValue{0.49f, 0.98f, 0.6624752f},
+                                TestCaseValue{0.67f, 0.51f, 0.4813684f},
+                                TestCaseValue{0.01f, 0.79f, 0.5179381f}),
+                         generateTestNameForValue);
 
 } // namespace pge::lattice
