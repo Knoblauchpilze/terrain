0552be25fe3195ef1a01fa848ae892d372045849
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-09-02 19:12:22 +0200

    feat: Tests for periodic gradient and perlin lattices

---
+++ tests/unit/lib/game/terrain/lattice/IPeriodicLatticePreparer.hh
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "ILattice.hh"
+#include "MockInterpolator.hh"
+#include <gmock/gmock.h>
+
+namespace pge::terrain {
+
+template<typename Lattice>
+class PeriodicLatticePreparer
+{
+  protected:
+  MockInterpolator *mockInterpolator{nullptr};
+
+  ILatticePtr lattice{};
+
+  void prepareLattice(const int period, const Seed seed)
+  {
+    auto interpolator = std::make_unique<::testing::NiceMock<MockInterpolator>>();
+    mockInterpolator  = interpolator.get();
+
+    lattice = std::make_unique<Lattice>(period, seed, std::move(interpolator));
+  }
+};
+
+} // namespace pge::terrain
