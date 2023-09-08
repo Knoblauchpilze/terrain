a4e0f0f92a57a426ccf0f15d0ba2a4e3166819e5
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-09-08 18:19:09 +0200

    feat: AbstractLattice3d also inherits from AbstractLattice

--- src/lib/game/terrain/lattice/AbstractLattice3d.hh
+++ src/lib/game/terrain/lattice/AbstractLattice3d.hh
@@ -1,16 +1,11 @@
 #pragma once
 
-#include "IAreaGenerator.hh"
-#include "IInterpolator.hh"
-#include "ILattice.hh"
-#include "IValueGenerator.hh"
-#include <functional>
-#include <optional>
+#include "AbstractLattice.hh"
 
 namespace pge::terrain {
 
 template<typename ValueType>
-class AbstractLattice3d : public ILattice<3>
+class AbstractLattice3d : public AbstractLattice<3, ValueType>
 {
   public:
   ~AbstractLattice3d() override = default;
@@ -18,17 +13,9 @@ class AbstractLattice3d : public ILattice<3>
   auto at(const Point3d &p) -> float override;
 
   protected:
-  using NormalizationFunc = std::function<float(const float)>;
-
   AbstractLattice3d(IValueGenerator3dPtr<ValueType> valueGenerator,
                     IInterpolator3dPtr interpolator,
                     std::optional<NormalizationFunc> normalization) noexcept;
-
-  private:
-  IArea3dGeneratorPtr m_areaGenerator;
-  IValueGenerator3dPtr<ValueType> m_valueGenerator;
-  IInterpolator3dPtr m_interpolator;
-  std::optional<NormalizationFunc> m_normalization;
 };
 
 } // namespace pge::terrain
