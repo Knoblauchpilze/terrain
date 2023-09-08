5272555c5663cbf000ab466e5ba24c8ec228840a
KnoblauchPilze <prolific-programmer@tuta.com>
2023-09-08 18:19:09 +0200

    feat: AbstractLattice3d also inherits from AbstractLattice

--- src/lib/game/terrain/lattice/AbstractLattice3d.hxx
+++ src/lib/game/terrain/lattice/AbstractLattice3d.hxx
@@ -13,17 +13,17 @@ AbstractLattice3d<ValueType>::AbstractLattice3d(
   IValueGenerator3dPtr<ValueType> valueGenerator,
   IInterpolator3dPtr interpolator,
   std::optional<NormalizationFunc> normalization) noexcept
-  : m_areaGenerator(std::make_unique<Area3dGenerator>())
-  , m_valueGenerator(std::move(valueGenerator))
-  , m_interpolator(std::move(interpolator))
-  , m_normalization(std::move(normalization))
+  : AbstractLattice<3, ValueType>(std::make_unique<Area3dGenerator>(),
+                                  std::move(valueGenerator),
+                                  std::move(interpolator),
+                                  std::move(normalization))
 {}
 
 template<typename ValueType>
 auto AbstractLattice3d<ValueType>::at(const Point3d &p) -> float
 {
   // https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/procedural-patterns-noise-part-1/creating-simple-1D-noise.html
-  const auto area = m_areaGenerator->areaSurrounding(p);
+  const auto area = this->m_areaGenerator->areaSurrounding(p);
 
   const auto &frontTopLeft     = area.points[Area3dGenerator::FRONT_TOP_LEFT];
   const auto &frontTopRight    = area.points[Area3dGenerator::FRONT_TOP_RIGHT];
@@ -35,15 +35,15 @@ auto AbstractLattice3d<ValueType>::at(const Point3d &p) -> float
   const auto &backBottomRight = area.points[Area3dGenerator::BACK_BOTTOM_RIGHT];
   const auto &backBottomLeft  = area.points[Area3dGenerator::BACK_BOTTOM_LEFT];
 
-  const auto ftl = m_valueGenerator->generateFor(frontTopLeft, p);
-  const auto ftr = m_valueGenerator->generateFor(frontTopRight, p);
-  const auto fbr = m_valueGenerator->generateFor(frontBottomRight, p);
-  const auto fbl = m_valueGenerator->generateFor(frontBottomLeft, p);
+  const auto ftl = this->m_valueGenerator->generateFor(frontTopLeft, p);
+  const auto ftr = this->m_valueGenerator->generateFor(frontTopRight, p);
+  const auto fbr = this->m_valueGenerator->generateFor(frontBottomRight, p);
+  const auto fbl = this->m_valueGenerator->generateFor(frontBottomLeft, p);
 
-  const auto btl = m_valueGenerator->generateFor(backTopLeft, p);
-  const auto btr = m_valueGenerator->generateFor(backTopRight, p);
-  const auto bbr = m_valueGenerator->generateFor(backBottomRight, p);
-  const auto bbl = m_valueGenerator->generateFor(backBottomLeft, p);
+  const auto btl = this->m_valueGenerator->generateFor(backTopLeft, p);
+  const auto btr = this->m_valueGenerator->generateFor(backTopRight, p);
+  const auto bbr = this->m_valueGenerator->generateFor(backBottomRight, p);
+  const auto bbl = this->m_valueGenerator->generateFor(backBottomLeft, p);
 
   const auto xRange = frontBottomRight(0) - frontBottomLeft(0);
   const auto yRange = backBottomLeft(1) - frontBottomLeft(1);
@@ -60,13 +60,13 @@ auto AbstractLattice3d<ValueType>::at(const Point3d &p) -> float
   data.deltas[Bilinear3d::Z]          = pz;
   data.deltas[Bilinear3d::Y]          = py;
 
-  const auto val = m_interpolator->interpolate(data);
-  if (!m_normalization)
+  const auto val = this->m_interpolator->interpolate(data);
+  if (!this->m_normalization)
   {
     return val;
   }
 
-  return (*m_normalization)(val);
+  return (*this->m_normalization)(val);
 }
 
 } // namespace pge::terrain
