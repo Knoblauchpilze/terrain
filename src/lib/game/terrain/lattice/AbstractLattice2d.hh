bab354b3822f3291dc5bc7951ee4ddcb4a732bd9
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2023-09-14 08:09:41 +0200

    feat: Groundwork for caching lattice points

--- src/lib/game/terrain/lattice/AbstractLattice2d.hh
+++ src/lib/game/terrain/lattice/AbstractLattice2d.hh
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "AbstractLattice.hh"
+#include <unordered_map>
 
 namespace pge::terrain {
 
@@ -17,6 +18,12 @@ class AbstractLattice2d : public AbstractLattice<2, ValueType>
   AbstractLattice2d(IValueGenerator2dPtr<ValueType> valueGenerator,
                     IInterpolator2dPtr interpolator,
                     std::optional<NormalizationFunc> normalization) noexcept;
+
+  private:
+  static constexpr auto MAX_CACHE_SIZE = 200;
+  std::unordered_map<std::string, ValueType> m_cache{};
+
+  auto fromCacheOrGenerate(const LatticePoint2d &lp, const Point2d &p) const -> float;
 };
 
 } // namespace pge::terrain
--- src/lib/game/terrain/lattice/AbstractLattice2d.hxx
+++ src/lib/game/terrain/lattice/AbstractLattice2d.hxx
@@ -20,7 +20,7 @@ AbstractLattice2d<ValueType>::AbstractLattice2d(
 {}
 
 template<typename ValueType>
-auto AbstractLattice2d<ValueType>::at(const Point2d &p) -> float
+inline auto AbstractLattice2d<ValueType>::at(const Point2d &p) -> float
 {
   // https://stackoverflow.com/questions/1120833/derived-template-class-access-to-base-class-member-data
   // https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/procedural-patterns-noise-part-1/creating-simple-1D-noise.html
@@ -31,10 +31,10 @@ auto AbstractLattice2d<ValueType>::at(const Point2d &p) -> float
   const auto &bottomRight = area.points[Area2dGenerator::BOTTOM_RIGHT];
   const auto &bottomLeft  = area.points[Area2dGenerator::BOTTOM_LEFT];
 
-  const auto tl = this->m_valueGenerator->generateFor(topLeft, p);
-  const auto tr = this->m_valueGenerator->generateFor(topRight, p);
-  const auto br = this->m_valueGenerator->generateFor(bottomRight, p);
-  const auto bl = this->m_valueGenerator->generateFor(bottomLeft, p);
+  const auto tl = this->fromCacheOrGenerate(topLeft, p);
+  const auto tr = this->fromCacheOrGenerate(topRight, p);
+  const auto br = this->fromCacheOrGenerate(bottomRight, p);
+  const auto bl = this->fromCacheOrGenerate(bottomLeft, p);
 
   const auto xRange = topRight(0) - topLeft(0);
   const auto yRange = topLeft(1) - bottomLeft(1);
@@ -53,6 +53,13 @@ auto AbstractLattice2d<ValueType>::at(const Point2d &p) -> float
   }
 
   return (*this->m_normalization)(val);
+}
+
+template<typename ValueType>
+inline auto AbstractLattice2d<ValueType>::fromCacheOrGenerate(const LatticePoint2d &lp,
+                                                              const Point2d &p) const -> float
+{
+  return this->m_valueGenerator->generateFor(lp, p);
 }
 
 } // namespace pge::terrain
